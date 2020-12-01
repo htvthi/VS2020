7e6fa905f8f035634d79b3f954f7fc6d4c5f0e5b
Gesa Boehm <gesa@toboehm.de>
2020-12-01 09:53:14 +0100

    Übung 3 Aufgabe 1 kommentiert

--- ueb3.synchronized/src/org/htw/fiw/vs/Name.java
+++ ueb3.synchronized/src/org/htw/fiw/vs/Name.java
@@ -5,15 +5,21 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Name {
 
+	// Zugangskontrolle für Threads realisieren: damit nur ein Thread gleichzeitig
+	// mit dem Objekt interagieren kann
 	private final Lock lock = new ReentrantLock();
 	private String first;
 	private String last;
 
 	public void set(String first, String last) {
 
-		System.out.println("#set() try to acquire lock " + Thread.currentThread().getName());
+		/*
+		 * Nur ein Thread hat Zugriff auf die Setter und die Getter-Methode, um zu
+		 * vermeiden, dass halbfertige Daten via Get gezogen werden können, während die
+		 * Set-Methode noch ausgeführt wird
+		 * 
+		 */
 		lock.lock();
-		System.out.println("#set() lock aquired " + Thread.currentThread().getName());
 
 		try {
 			this.first = first;
@@ -23,24 +29,25 @@ public class Name {
 				e.printStackTrace();
 			}
 			this.last = last;
+			/*
+			 * Wenn es eine Exception innerhalb des try-Blocks gibt, wollen wir trotzdem das
+			 * Lock releasen, damit das Programm weiter läuft
+			 */
 		} finally {
 			lock.unlock();
-			System.out.println("#set() lock released " + Thread.currentThread().getName());
 
 		}
 
 	}
 
 	public String get() {
-		System.out.println("#get() try to acquire lock " + Thread.currentThread().getName());
 		lock.lock();
-		System.out.println("#get()lock acquired " + Thread.currentThread().getName());
 
 		try {
 			return this.first + " " + this.last;
 		} finally {
 			lock.unlock();
-			System.out.println("#get() lock released " + Thread.currentThread().getName());
+
 		}
 	}
 
